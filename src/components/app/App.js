ab7b785027791c05dbd5594489bbf99c8cff62ef
LukasKaselionis <l.kaselionis@gmail.com>
2020-03-06 10:14:25 +0200

    adding imageList component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import SearchBar from '../searchBar/SearchBar'
+import ImageList from '../imageList/ImageList';
 import './App.css';
 
 function App() {
@@ -7,6 +8,9 @@ function App() {
     <div className="App">
       <div className="App-header">
           <SearchBar></SearchBar>
+      </div>
+      <div className="App-content">
+          <ImageList></ImageList>
       </div>
     </div>
   );
---
+++ src/components/imageList/ImageList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ImageList.css';
+
+function ImageList(){
+    return (
+        <div className="images">
+            <div className="images-list">
+
+            </div>
+        </div>
+    );
+}
+
+export default ImageList;
